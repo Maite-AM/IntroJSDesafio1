d065e7159668bd8726e06b157b99a4ad11aef908
Maite-AM <maite.astete@gmail.com>
2024-03-02 17:15:16 -0300

    Upload desafío

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+const precio = 400000
+
+
+function aumentar(){
+    const precioTotalElemento = document.querySelector('.valor-total');
+    const cantidadElemento = document.querySelector('.cantidad'); 
+    const cantidadValor = Number(document.querySelector('.cantidad').innerHTML);
+    const nuevaCantidad = cantidadValor + 1;
+
+    cantidadElemento.innerHTML = nuevaCantidad;
+
+    const valorTotal = calcularMontoTotal(nuevaCantidad,precio);
+
+    precioTotalElemento.innerHTML = valorTotal;
+
+}
+
+function disminuir(){
+    const precioTotalElemento = document.querySelector('.valor-total');
+    const cantidadElemento = document.querySelector('.cantidad'); 
+    const cantidadValor = Number(document.querySelector('.cantidad').innerHTML); 
+
+    if(cantidadValor > 0) {
+        const nuevaCantidad = cantidadValor - 1;
+        cantidadElemento.innerHTML = nuevaCantidad;
+
+        const valorTotal = calcularMontoTotal(nuevaCantidad,precio);
+        precioTotalElemento.innerHTML = valorTotal;
+     }
+     
+}
+
+const precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = Intl.NumberFormat('es-CL', {
+    style:'currency',
+    currency:'CLP',
+    currencySign:'accounting',
+    maximumSignificantDigits:2,
+}).format(precio);
